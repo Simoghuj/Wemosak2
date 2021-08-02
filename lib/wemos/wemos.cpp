b98a4af4cc041ef00ecd5e6b165749fd18545466
Simoghuj <skladsim@seznam.cz>
2021-08-02 11:47:06 +0200

    added adafruit libraries

--- lib/wemos/wemos.cpp
+++ lib/wemos/wemos.cpp
@@ -4,6 +4,21 @@ void wemos::setup(){
     
 }
 
-void wemos::oled(){
-    Serial.print("hello");
+void wemos::oledbegin(){
+    if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+
+  display.display();
+  delay(2000); 
+ 
+  display.clearDisplay();
+
+  display.setTextSize(1);             // Normal 1:1 pixel scale
+  display.setTextColor(SSD1306_WHITE);        // Draw white text
+  display.setCursor(0,0);             // Start at top-left corner
+  display.print(F("Hello World!!"));
+
+
 }
